a58909792a717cc79ca93f83c7bd2d24b124cef1
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-08-23 15:18:02 +0330

    dead code ftw

--- process.py
+++ process.py
@@ -8,12 +8,12 @@ new_text = text.replace('\n', '')
 
 print(new_text)
 
-dataset = {}
-dataset['text'] = new_text
+# dataset = {}
+# dataset['text'] = new_text
 
-dataset = json.dumps(dataset)
+# dataset = json.dumps(dataset)
 
-final_set = open('dataset.jsonl', 'w')
-final_set.write(dataset)
+# final_set = open('dataset.jsonl', 'w')
+# final_set.write(dataset)
 
 print('created')
